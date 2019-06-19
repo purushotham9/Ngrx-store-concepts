3e152ac73cefa4af3198c0d4da40daa6c5d182cd
kvanaIndia <kvanaindia@kvanaIndias-iMac.local>
2019-06-19 19:33:14 +0530

    using ngrx store concepts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+
+import { IAppState } from './store/states/app.state';
+import { GetConfig } from './store/actions/config.actions';
+import { selectConfig } from './store/selectors/config.selector';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-ngrx-example';
+  config$ = this._store.pipe(select(selectConfig));
+
+  constructor(private _store: Store<IAppState>) {}
+
+  ngOnInit() {
+    this._store.dispatch(new GetConfig());
+  }
 }
